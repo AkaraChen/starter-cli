cf9722579c2c3a827ddbe43228fdbd2b1440ed1e
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-07-02 19:23:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# starter-cli
+
+## Set up
+
+1. Open `package.json`, edit `bin.starter` **!important**
+2. Run `npm link`
+
+## Develop
+
+Just run `npm run dev`, it's `HMR` ready, your cli will be compiled immediately.
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+import shell from 'shelljs'
+
+const program = new Command()
+
+program
+    .name('akr-cli')
+    .description('CLI to some utilities')
+    .version('1.0.0')
+
+program.command("greet")
+    .action(() => {
+        shell.echo("Hello, World!")
+    })
+
+program.parse()
